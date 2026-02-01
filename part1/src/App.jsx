fcbcdd7f05339e58dd5a7bd1e3a81e0e123b6e0b
Akira <DiegoAkiraAlvaradoArroyo@gmail.com>
2026-01-31 20:47:31 -0500

    Adding Exercises 1.1 and 1.2

---
+++ part1/src/App.jsx
@@ -0,0 +1,27 @@
+import Header from "./Header"
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      excercises: 7
+    },
+    {
+      name: 'State of a component',
+      excercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course}>
+    </div>
+  )
+}
+
+export default App
