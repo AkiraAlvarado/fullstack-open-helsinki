45afc0a8ed07d1b9e02b4f15a14f8c186dfab991
Akira <DiegoAkiraAlvaradoArroyo@gmail.com>
2026-04-10 18:21:48 -0500

    Add the exercises 2.6 - 2.10 the phonebook

---
+++ part 2/the-phonebook/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+
+import Filter from './Filter'
+import PersonForm from './PersonForm'
+import Persons from './Persons'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+
+  const addName = (event) => {
+    event.preventDefault()
+
+    const alreadyExists = persons.some(
+      person => person.name.toLowerCase() === newName.toLowerCase()
+    )
+
+    if (alreadyExists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const newPerson = {
+      id: persons.length + 1,
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons(persons.concat(newPerson))
+
+    setNewName('')
+    setNewNumber('')
+  }
+
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+      />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        addName={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+
+export default App
